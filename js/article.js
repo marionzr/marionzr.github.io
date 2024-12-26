38dfa8e1e77726c9cfe45221d56d28e40588def4
Mario <47438990+marionzr@users.noreply.github.com>
2024-12-27 00:14:09 +0100

    feat(articles): display article creation date
    - Show the **creation date** of each article for better context.
    - Enhance metadata visibility to improve user experience.
    
    Tags: Articles, UI, Metadata

--- js/article.js
+++ js/article.js
@@ -1,5 +1,7 @@
 (function () { // IIFE
 
+    const app = globalThis.app;
+
     function checkMissingInformation() {
         try {
             const documentTitle = document.title;
@@ -36,11 +38,46 @@
         }
     }
 
-    function init() {
-        checkMissingInformation();
+    function getCurrentFileName() {
+        const path = window.location.pathname;
+        return getFileName(path);
     }
 
-    document.addEventListener('DOMContentLoaded', init);
+    function getFileName(fullFilename) {
+        return fullFilename.substring(fullFilename.lastIndexOf('/') + 1);
+    }
+
+    async function loadCreatedAtDateTime() {
+        try {
+            const dateElement = document.getElementById('article-header-info-matadata-created-at');
+
+            const filename = getCurrentFileName();
+            const articles = await sources.getArticlesMetaDataAsync();
+
+            if (!articles) {
+                console.warn('No articles returned');
+                return;
+            }
+
+            const currentArticle = articles.find(a => getFileName(a.filename) == filename);
+
+            if (!currentArticle) {
+                console.warn(`No metadata found for article '${filename}'`);
+                return
+            }
+            const createdAt = app.formatDate(new Date(currentArticle.created_at));
+            dateElement.textContent = createdAt;
+        } catch (error) {
+            app.onError(error, 'Error in setArticlePublishDate');
+        }
+    }
+
+    async function init() {
+        checkMissingInformation();
+        await loadCreatedAtDateTime();
+    }
+
+    document.addEventListener('DOMContentLoaded', () => init());
 })();
 
 function getHomeLink() {
